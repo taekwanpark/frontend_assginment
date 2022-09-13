5fdb28c8d63b1b4c645ba4ba1bfb82de7c01c8e5
Park Taekwan <75081212+techighpark@users.noreply.github.com>
2022-09-13 16:23:18 +0900

    install vuex@next

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Frontend - Assignment #1</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
