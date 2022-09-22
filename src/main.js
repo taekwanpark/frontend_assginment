460c0bfe6155614e640903f94353e3bb677dd80c
taekwanpark <techigh@amuz.co.kr>
2022-09-22 14:42:29 +0900

    c

--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import store from './store';
 import './index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
-import { faHourglass } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faHourglass, faUserSecret } from '@fortawesome/free-solid-svg-icons';
 
-const app = createApp(App);
-library.add(faUserSecret);
-library.add(faCheck);
-library.add(faHourglass);
-app.component('font-awesome-icon', FontAwesomeIcon);
-app.use(store).use(router).mount('#app');
+const app = createApp( App );
+library.add( faUserSecret );
+library.add( faCheck );
+library.add( faHourglass );
+app.component( 'font-awesome-icon', FontAwesomeIcon );
+app
+	.use( store )
+	.use( router )
+	.mount( '#app' );
